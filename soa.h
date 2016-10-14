68774b2b7496802684b4d390bf6ee69c4f8e5094
Shanee Nishry <shanee@shanee.io>
2016-10-13 22:54:52 -0700

    Added asserts and safety checks.

--- soa.h
+++ soa.h
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <cassert>
 #include <type_traits>
 #include <vector>
 
 // This class makes heavy use of template parameter pack.
 // http://en.cppreference.com/w/cpp/language/parameter_pack
+
+#define FOR_EACH_ARRAY(expression)                                          \
+  size_t array_index = 0;                                                   \
+  int dummy[] = {                                                           \
+      (get_array<Elements>(array_index)->expression, ++array_index, 0)...}; \
+  (void)dummy
 
 // Structure of Arrays.
 template <typename... Elements>
@@ -12,18 +19,13 @@ class SoA {
  public:
   SoA() {
     size_t array_index = 0;
-    int dummy_init[] = {(new (get_array<Elements>(array_index++))
+    int dummy_init[] = {(new (get_array<Elements>(array_index))
                              std::vector<Elements>(),
-                         0)...};
+                         ++array_index++, 0)...};
     (void)dummy_init;  // avoids unused variable compiler warnings.
   }
 
-  virtual ~SoA() {
-    size_t array_index = 0;
-    int dummy[] = {
-        (get_array<Elements>(array_index++)->~vector<Elements>(), 0)...};
-    (void)dummy;
-  }
+  virtual ~SoA() { FOR_EACH_ARRAY(~vector<Elements>()); }
 
   // Returns the number of elements in the arrays.
   size_t size() const { return size_; }
@@ -33,10 +35,7 @@ class SoA {
 
   // Adds an element to the end of the arrays via rvalue.
   void push_back(Elements &&... elements) {
-    size_t array_index = 0;
-    int dummy[] = {(
-        push_back_impl(std::forward<Elements>(elements), array_index++), 0)...};
-    (void)dummy;
+    FOR_EACH_ARRAY(push_back(std::forward<Elements>(elements)));
 
     // Increment number of elements in the arrays.
     ++size_;
@@ -44,9 +43,7 @@ class SoA {
 
   // Adds an element to the end of the arrays, as a copy of the const ref value.
   void push_back(const Elements &... elements) {
-    size_t array_index = 0;
-    int dummy[] = {(push_back_impl(elements, array_index++), 0)...};
-    (void)dummy;
+    FOR_EACH_ARRAY(push_back(elements));
 
     // Increment number of elements in the arrays.
     ++size_;
@@ -54,9 +51,11 @@ class SoA {
 
   // Erases the last element in the arrays.
   void pop_back() {
-    size_t array_index = 0;
-    int dummy[] = {(pop_back_impl<Elements>(array_index++), 0)...};
-    (void)dummy;
+    if (empty()) {
+      return;
+    }
+
+    FOR_EACH_ARRAY(pop_back());
 
     // Decrement number of elements in the arrays.
     --size_;
@@ -64,6 +63,11 @@ class SoA {
 
   // Erases elements of a given index from the arrays.
   void erase(size_t element_index) {
+    if (element_index >= size()) {
+      assert(true);
+      return;
+    }
+
     size_t array_index = 0;
     int dummy[] = {(erase_impl<Elements>(element_index, array_index++), 0)...};
     (void)dummy;
@@ -74,6 +78,12 @@ class SoA {
 
   // Erases |num_elements| elements from |start_index| from the arrays.
   void erase(size_t start_index, size_t num_elements) {
+    if (start_index >= size() || (start_index + num_elements) > size()) {
+      assert(true);
+
+      return;
+    }
+
     size_t array_index = 0;
     int dummy[] = {
         (erase_impl<Elements>(start_index, num_elements, array_index++), 0)...};
@@ -85,24 +95,23 @@ class SoA {
 
   // Swaps two elements inside the array.
   void swap(size_t index0, size_t index1) {
+    if (index0 >= size() || index1 > size()) {
+      assert(true);
+
+      return;
+    }
+
     size_t array_index = 0;
     int dummy[] = {(swap_impl<Elements>(index0, index1, array_index++), 0)...};
     (void)dummy;
   }
 
   // Reserves enough memories in all arrays to contain |reserve_size| elements.
-  void reserve(size_t reserve_size) {
-    // Reserve arrays. Dummy does pack expansion via list-initialization.
-    size_t array_index = 0;
-    int dummy[] = {(reserve_impl<Elements>(reserve_size, array_index++), 0)...};
-    (void)dummy;
-  }
+  void reserve(size_t reserve_size) { FOR_EACH_ARRAY(reserve(reserve_size)); }
 
   // Resizes the arrays to contain |size| elements;
   void resize(size_t size) {
-    size_t array_index = 0;
-    int dummy[] = {(resize_impl<Elements>(size, array_index++), 0)...};
-    (void)dummy;
+    FOR_EACH_ARRAY(resize(size));
 
     size_ = size;
   }
@@ -110,16 +119,18 @@ class SoA {
   // Returns a pointer to the |ArrayIndex|th array.
   template <typename ElementType, std::size_t ArrayIndex>
   ElementType *array() {
-    std::vector<ElementType> *array = get_array<ElementType>(ArrayIndex);
+    static_assert(ArrayIndex < kNumArrays, "Requested invalid array index.");
 
+    std::vector<ElementType> *array = get_array<ElementType>(ArrayIndex);
     return array->data();
   }
 
   // Returns a const pointer to the |ArrayIndex|th array.
   template <typename ElementType, std::size_t ArrayIndex>
   const ElementType *array() const {
-    const std::vector<ElementType> *array = get_array<ElementType>(ArrayIndex);
+    static_assert(ArrayIndex < kNumArrays, "Requested invalid array index.");
 
+    const std::vector<ElementType> *array = get_array<ElementType>(ArrayIndex);
     return array->data();
   }
 
@@ -127,8 +138,10 @@ class SoA {
   // as type |ElementType|.
   template <typename ElementType, std::size_t ArrayIndex>
   ElementType &get(size_t index) {
-    std::vector<ElementType> *array = get_array<ElementType>(ArrayIndex);
+    static_assert(ArrayIndex < kNumArrays,
+                  "Requested invalid array index in get().");
 
+    std::vector<ElementType> *array = get_array<ElementType>(ArrayIndex);
     return (*array)[index];
   }
 
@@ -136,49 +149,39 @@ class SoA {
   // array as type |ElementType|.
   template <typename ElementType, std::size_t ArrayIndex>
   const ElementType &get(size_t index) const {
-    const std::vector<ElementType> *array = get_array<ElementType>(ArrayIndex);
+    static_assert(ArrayIndex < kNumArrays,
+                  "Requested invalid array index in get().");
 
+    const std::vector<ElementType> *array = get_array<ElementType>(ArrayIndex);
     return (*array)[index];
   }
 
   // Returns the number of arrays.
-  size_t num_arrays() const { return sizeof...(Elements); }
+  size_t num_arrays() const { return kNumArrays; }
 
  private:
-  size_t size_ = 0;
-  using ArrayType = std::aligned_storage<sizeof(std::vector<void *>),
-                                         alignof(std::vector<void *>)>::type;
-  ArrayType arrays_[sizeof...(Elements)];
-
   template <class Type>
   std::vector<Type> *get_array(size_t array_index) {
+    static_assert(sizeof(std::vector<Type>) == sizeof(std::vector<void *>),
+                  "Structure of Array assumes of vector<Type> is same size as "
+                  "vector<void*> however the assumption failed.");
+    static_assert(alignof(std::vector<Type>) == alignof(std::vector<void *>),
+                  "Structure of Array assumes of vector<Type> is same "
+                  "alignment as vector<void*> however the assumption failed.");
+
     return reinterpret_cast<std::vector<Type> *>(&arrays_[array_index]);
   }
 
   template <class Type>
   const std::vector<Type> *get_array(size_t array_index) const {
+    static_assert(sizeof(std::vector<Type>) == sizeof(std::vector<void *>),
+                  "Structure of Array assumes of vector<Type> is same size as "
+                  "vector<void*> however the assumption failed.");
+    static_assert(alignof(std::vector<Type>) == alignof(std::vector<void *>),
+                  "Structure of Array assumes of vector<Type> is same "
+                  "alignment as vector<void*> however the assumption failed.");
+
     return reinterpret_cast<const std::vector<Type> *>(&arrays_[array_index]);
-  }
-
-  template <class Type>
-  void push_back_impl(Type &&element, size_t array_index) {
-    std::vector<Type> *array = get_array<Type>(array_index);
-
-    array->push_back(std::forward<Type>(element));
-  }
-
-  template <class Type>
-  void push_back_impl(const Type &element, size_t array_index) {
-    std::vector<Type> *array = get_array<Type>(array_index);
-
-    array->push_back(element);
-  }
-
-  template <class Type>
-  void pop_back_impl(size_t array_index) {
-    std::vector<Type> *array = get_array<Type>(array_index);
-
-    array->pop_back();
   }
 
   template <class Type>
@@ -205,20 +208,12 @@ class SoA {
     swap((*array)[index0], (*array)[index1]);
   }
 
-  template <class Type>
-  void reserve_impl(size_t size, size_t array_index) {
-    std::vector<Type> *array = get_array<Type>(array_index);
-
-    array->reserve(size);
-  }
-
-  template <class Type>
-  void resize_impl(size_t size, size_t array_index) {
-    std::vector<Type> *array = get_array<Type>(array_index);
-
-    array->resize(size);
-  }
-
   SoA(const SoA &) = delete;
   SoA &operator=(const SoA &) = delete;
+
+  using ArrayType = std::aligned_storage<sizeof(std::vector<void *>),
+                                         alignof(std::vector<void *>)>::type;
+  static const size_t kNumArrays = sizeof...(Elements);
+  size_t size_ = 0;
+  ArrayType arrays_[kNumArrays];
 };
