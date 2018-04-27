09085a5785524b300156bfde61ccc88437ba4562
Shanee Nishry <shanee@shanee.io>
2018-04-27 00:58:43 -0700

    Updated from local engine version. Sorry for lacking description, this been a while. :)

--- soa.h
+++ soa.h
@@ -1,21 +1,24 @@
 #pragma once
 
+#include <stddef.h>
 #include <cassert>
+#include <tuple>
 #include <type_traits>
 #include <vector>
 
 // This class makes heavy use of template parameter pack.
 // http://en.cppreference.com/w/cpp/language/parameter_pack
 
-#define FOR_EACH_ARRAY(expression)                                             \
-  size_t array_index = 0;                                                      \
-  int dummy[] = {                                                              \
-      (get_array<Elements>(array_index)->expression, ++array_index, 0)...};    \
+#define FOR_EACH_ARRAY(expression)                                          \
+  size_t array_index = 0;                                                   \
+  int dummy[] = {                                                           \
+      (get_array<Elements>(array_index)->expression, ++array_index, 0)...}; \
   (void)dummy
 
 // Structure of Arrays.
-template <typename... Elements> class SoA {
-public:
+template <typename... Elements>
+class SoA {
+ public:
   // Helper to deduce the type of the Nth element.
   template <size_t N>
   using NthTypeOf =
@@ -26,10 +29,49 @@ public:
     int dummy_init[] = {(new (get_array<Elements>(array_index))
                              std::vector<Elements>(),
                          ++array_index, 0)...};
-    (void)dummy_init; // avoids unused variable compiler warnings.
+    (void)dummy_init;  // avoids unused variable compiler warnings.
   }
 
-  virtual ~SoA() { FOR_EACH_ARRAY(~vector<Elements>()); }
+  SoA(const SoA& other) : SoA() { *this = other; }
+
+  SoA(SoA&& other) {
+    size_t idx = 0;
+    int dummy_init[] = {(new (get_array<Elements>(idx)) std::vector<Elements>(
+                             std::move(*other.get_array<Elements>(idx))),
+                         ++idx, 0)...};
+    (void)dummy_init;  // avoids unused variable compiler warnings.
+
+    size_ = other.size_;
+    other.size_ = 0;
+  }
+
+  virtual ~SoA() {
+    using std::vector;
+    FOR_EACH_ARRAY(~vector<Elements>());
+  }
+
+  SoA& operator=(const SoA& other) {
+    size_t idx = 0;
+    int dummy_init[] = {
+        (*get_array<Elements>(idx) = *other.get_array<Elements>(idx), ++idx,
+         0)...};
+    (void)dummy_init;  // avoids unused variable compiler warnings.
+
+    size_ = other.size_;
+    return *this;
+  }
+
+  SoA& operator=(SoA&& other) {
+    size_t idx = 0;
+    int dummy_init[] = {
+        (*get_array<Elements>(idx) = std::move(*other.get_array<Elements>(idx)),
+         ++idx, 0)...};
+    (void)dummy_init;  // avoids unused variable compiler warnings.
+
+    size_ = other.size_;
+    other.size_ = 0;
+    return *this;
+  }
 
   // Returns the number of elements in the arrays.
   size_t size() const { return size_; }
@@ -38,15 +80,23 @@ public:
   bool empty() const { return (size_ == 0); }
 
   // Adds an element to the end of the arrays via rvalue.
-  void push(Elements &&... elements) {
+  void push(Elements&&... elements) {
     FOR_EACH_ARRAY(emplace_back(std::forward<Elements>(elements)));
 
     // Increment number of elements in the arrays.
     ++size_;
   }
 
+  void replace(const size_t index, Elements&&... elements) {
+    FOR_EACH_ARRAY(operator[](index) = std::forward<Elements>(elements));
+  }
+
+  void replace(const size_t index, const Elements&... elements) {
+    FOR_EACH_ARRAY(operator[](index) = elements);
+  }
+
   // Adds an element to the end of the arrays, as a copy of the const ref value.
-  void push_back(const Elements &... elements) {
+  void push_back(const Elements&... elements) {
     FOR_EACH_ARRAY(push_back(elements));
 
     // Increment number of elements in the arrays.
@@ -125,44 +175,47 @@ public:
   }
 
   // Returns a pointer to the |ArrayIndex|th array.
-  template <std::size_t ArrayIndex> NthTypeOf<ArrayIndex> *array() {
+  template <size_t ArrayIndex>
+  NthTypeOf<ArrayIndex>* array() {
     static_assert(ArrayIndex < kNumArrays, "Requested invalid array index.");
 
     using ElementType = NthTypeOf<ArrayIndex>;
-    std::vector<ElementType> *array = get_array<ElementType>(ArrayIndex);
+    std::vector<ElementType>* array = get_array<ElementType>(ArrayIndex);
 
     return array->data();
   }
 
   // Returns a const pointer to the |ArrayIndex|th array.
-  template <std::size_t ArrayIndex> const NthTypeOf<ArrayIndex> *array() const {
+  template <size_t ArrayIndex>
+  const NthTypeOf<ArrayIndex>* array() const {
     static_assert(ArrayIndex < kNumArrays, "Requested invalid array index.");
 
     using ElementType = NthTypeOf<ArrayIndex>;
-    const std::vector<ElementType> *array = get_array<ElementType>(ArrayIndex);
+    const std::vector<ElementType>* array = get_array<ElementType>(ArrayIndex);
 
     return array->data();
   }
 
-  template <std::size_t ArrayIndex> NthTypeOf<ArrayIndex> &get(size_t index) {
+  template <size_t ArrayIndex>
+  NthTypeOf<ArrayIndex>& get(size_t index) {
     static_assert(ArrayIndex < kNumArrays,
                   "Requested invalid array index in get().");
 
     using ElementType = NthTypeOf<ArrayIndex>;
-    std::vector<ElementType> *array = get_array<ElementType>(ArrayIndex);
+    std::vector<ElementType>* array = get_array<ElementType>(ArrayIndex);
 
     return (*array)[index];
   }
 
   // Returns a const reference to the |index|th element from the |ArrayIndex|th
   // array as type |ElementType|.
-  template <typename std::size_t ArrayIndex>
-  const NthTypeOf<ArrayIndex> &get(size_t index) const {
+  template <size_t ArrayIndex>
+  const NthTypeOf<ArrayIndex>& get(size_t index) const {
     static_assert(ArrayIndex < kNumArrays,
                   "Requested invalid array index in get().");
 
     using ElementType = NthTypeOf<ArrayIndex>;
-    const std::vector<ElementType> *array = get_array<ElementType>(ArrayIndex);
+    const std::vector<ElementType>* array = get_array<ElementType>(ArrayIndex);
 
     return (*array)[index];
   }
@@ -170,33 +223,34 @@ public:
   // Returns the number of arrays.
   size_t num_arrays() const { return kNumArrays; }
 
-private:
-  template <class Type> std::vector<Type> *get_array(size_t array_index) {
-    static_assert(sizeof(std::vector<Type>) == sizeof(std::vector<void *>),
+ private:
+  template <class Type>
+  std::vector<Type>* get_array(size_t array_index) {
+    static_assert(sizeof(std::vector<Type>) == sizeof(std::vector<void*>),
                   "Structure of Array assumes of vector<Type> is same size as "
                   "vector<void*> however the assumption failed.");
-    static_assert(alignof(std::vector<Type>) == alignof(std::vector<void *>),
+    static_assert(alignof(std::vector<Type>) == alignof(std::vector<void*>),
                   "Structure of Array assumes of vector<Type> is same "
                   "alignment as vector<void*> however the assumption failed.");
 
-    return reinterpret_cast<std::vector<Type> *>(&arrays_[array_index]);
+    return reinterpret_cast<std::vector<Type>*>(&arrays_[array_index]);
   }
 
   template <class Type>
-  const std::vector<Type> *get_array(size_t array_index) const {
-    static_assert(sizeof(std::vector<Type>) == sizeof(std::vector<void *>),
+  const std::vector<Type>* get_array(size_t array_index) const {
+    static_assert(sizeof(std::vector<Type>) == sizeof(std::vector<void*>),
                   "Structure of Array assumes of vector<Type> is same size as "
                   "vector<void*> however the assumption failed.");
-    static_assert(alignof(std::vector<Type>) == alignof(std::vector<void *>),
+    static_assert(alignof(std::vector<Type>) == alignof(std::vector<void*>),
                   "Structure of Array assumes of vector<Type> is same "
                   "alignment as vector<void*> however the assumption failed.");
 
-    return reinterpret_cast<const std::vector<Type> *>(&arrays_[array_index]);
+    return reinterpret_cast<const std::vector<Type>*>(&arrays_[array_index]);
   }
 
   template <class Type>
   void erase_impl(size_t element_index, size_t array_index) {
-    std::vector<Type> *array = get_array<Type>(array_index);
+    std::vector<Type>* array = get_array<Type>(array_index);
 
     array->erase(array->begin() + element_index);
   }
@@ -204,7 +258,7 @@ private:
   template <class Type>
   void erase_impl(size_t element_index, size_t num_elements,
                   size_t array_index) {
-    std::vector<Type> *array = get_array<Type>(array_index);
+    std::vector<Type>* array = get_array<Type>(array_index);
 
     array->erase(array->begin() + element_index,
                  array->begin() + element_index + num_elements);
@@ -212,17 +266,14 @@ private:
 
   template <class Type>
   void swap_impl(size_t index0, size_t index1, size_t array_index) {
-    std::vector<Type> *array = get_array<Type>(array_index);
+    std::vector<Type>* array = get_array<Type>(array_index);
 
     using std::swap;
     swap((*array)[index0], (*array)[index1]);
   }
 
-  SoA(const SoA &) = delete;
-  SoA &operator=(const SoA &) = delete;
-
-  using ArrayType = std::aligned_storage<sizeof(std::vector<void *>),
-                                         alignof(std::vector<void *>)>::type;
+  using ArrayType = std::aligned_storage<sizeof(std::vector<void*>),
+                                         alignof(std::vector<void*>)>::type;
   static const size_t kNumArrays = sizeof...(Elements);
   size_t size_ = 0;
   ArrayType arrays_[kNumArrays];
